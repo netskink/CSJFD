13b40f0d0c008f8a988506a30d7b3678e98b4ef0
John Davis <davisjf@gmail.com>
2017-07-29 10:58:14 -0400

    build check is clean now.

--- R/csjfd.R
+++ R/csjfd.R
@@ -6,16 +6,18 @@
 library(httr)
 
 # use the hashmap library
-library(hashmap)
+#library(hashmap)
 
-hello <- function() {
-  print("Hello, world!")
-}
+# from check of package build
+#importFrom("stats", "rnorm")
 
+
+# To remove warnings during check build, the docs were added to man getData.Rd
+#
 getData <- function(some_url) {
   cat("getData called with url ", some_url, "\n")
   cat("Fetching url .....\n")
-  url_page_response = GET(some_url)
+  url_page_response = httr::GET(some_url)
   return (url_page_response)
 
 }
@@ -27,6 +29,6 @@ myTest1 <- function() {
 myTest2 <- function() {
 
   set.seed(123)
-  (HH <- hashmap(LETTERS, rnorm(26)))
-  print(HH[[c("A", "B")]])
+#  (HH <- hashmap(LETTERS, rnorm(26)))
+#  print(HH[[c("A", "B")]])
 }
