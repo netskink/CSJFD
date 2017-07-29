09a7abf85c6cb284d3a332dacfb998489d6cf3b3
John Davis <davisjf@gmail.com>
2017-07-29 09:02:29 -0400

    Adding new files from first test non git project

---
+++ R/csjfd.R
@@ -0,0 +1,20 @@
+# Use the httr lib to add custom header
+# see https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
+#
+# install.packages("httr")
+library(httr)
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+getData <- function(some_url) {
+  cat("getData called with parameter ", some_url, "\n")
+  url_page_response = GET(some_url)
+  cat(url_page_response)
+
+}
+
+myTest <- function() {
+  getData("http://echo.jsontest.com/fieldkey/fieldvalue/purpose/test")
+}
